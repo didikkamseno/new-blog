7f6e3d0a7e926e8c661d4cb8ace7cf035f01f73f
Kapil Chaudhary <kapil.math.hons@gmail.com>
2022-07-27 15:50:31 +0530

    waving-hand-animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import Container from '../components/Container';
 import BlogPostCard from '../components/BlogPostCard';
 // import Subscribe from '../components/Subscribe';
-import VideoCard from '../components/VideoCard';
+// import VideoCard from '../components/VideoCard';
 import TypedBios from 'components/TypedBios';
 
 export default function Home() {
@@ -16,7 +16,7 @@ export default function Home() {
             <div className="flex flex-col-reverse sm:flex-row items-start">
               <div className="flex flex-col pr-8">
                 <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-                  Kapil Chaudhary
+                  Kapil Chaudhary <span role='img' aria-label='waving-hand' className='wave animate-waving-hand'>👋</span>
                 </h1>
                 <h2 className="text-gray-700 dark:text-gray-200 mb-4">
                   A research scholar in fractional-order mathematical modelling working as{' '}
@@ -26,7 +26,8 @@ export default function Home() {
                 <p className="text-gray-600 dark:text-gray-400 mb-16">
                 </p>
               </div>
-              <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">
+              <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto rounded-full bg-gradient-to-r p-[2px] from-[#7928ca] to-[#ff0080]">
+                <div className="block bg-white dark:bg-dark rounded-full p-[2px]">
                 <Image
                   alt="Kapil Chaudhary"
                   height={176}
@@ -34,8 +35,9 @@ export default function Home() {
                   src="/avatar.jpg"
                   sizes="30vw"
                   priority
-                  className="filter grayscale-0 hover:grayscale-50 rounded-full sm:rounded-xl"
+                  className="filter rounded-full grayscale-0 hover:grayscale-50"
                 />
+                </div>
               </div>
             </div>
 
@@ -43,23 +45,23 @@ export default function Home() {
               Featured Posts
             </h3>
             <div className="flex gap-6 flex-col md:flex-row">
-              {/* <BlogPostCard
-                title="Everything I Know About Style Guides, Design Systems, and Component Libraries"
-                slug="style-guides-component-libraries-design-systems"
+              <BlogPostCard
+                title="CSIR NEWFMS Portal Guide"
+                slug="csir-newfms-guide-1"
                 gradient="from-[#D8B4FE] to-[#818CF8]"
               />
-              <BlogPostCard
+              {/* <BlogPostCard
                 title="Rust Is The Future of JavaScript Infrastructure"
-                slug="rust"
+                slug=""
                 gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
-              />
-              <BlogPostCard
-                title="Past, Present, and Future of React State Management"
-                slug="react-state-management"
-                gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
               /> */}
+              <BlogPostCard
+                title="Practice Questions"
+                slug="practice-questions-matrix-algebra-mat411pr"
+                gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
+              /> 
             </div>
-            <Link href="/blog">
+            {/* <Link href="/blog">
               <a className="flex mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6">
                 Read all posts
                 <svg
@@ -79,7 +81,7 @@ export default function Home() {
               </a>
             </Link>
 
-            <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-16 text-black dark:text-white">
+            {/* <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-16 text-black dark:text-white">
               Learn React & Next.js
             </h3>
             <p className="text-gray-600 dark:text-gray-400 mb-4">
@@ -132,9 +134,9 @@ export default function Home() {
                   d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
                 />
               </svg>
-            </a>
+            </a> 
             <span className="h-16" />
-            {/* <Subscribe /> */}
+             <Subscribe /> */}
           </div>
         </Container>
         </Suspense>
