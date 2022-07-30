e3a9dfbc21e981b775fd0100f1d29382e0fe596d
Kapil Chaudhary <kapil.math.hons@gmail.com>
2022-07-30 17:07:44 +0530

    menu-side-bar

---
+++ components/menu/NavLink.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function NavLink({ children, href, name }) {
+  const router = useRouter();
+  const isActive = router.asPath === href;
+  return (
+    <Link passHref href={href}>
+      <div
+        className={`${
+          isActive
+            ? "font-semibold text-zinc-600 dark:text-white bg-neutral-50 dark:bg-neutral-600"
+            : ""
+        } navLink flex items-center space-x-4 cursor-pointer hover:bg-neutral-50 dark:hover:bg-neutral-600 rounded-lg p-2`}
+      >
+        {children}
+        <p className="block">{name}</p>
+      </div>
+    </Link>
+  );
+}
